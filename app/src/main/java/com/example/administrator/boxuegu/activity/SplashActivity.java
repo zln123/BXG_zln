489b828314d6d0d7b2551be42efbb4e0ef37ec5d
zln <1377046392@qq.com>
2019-03-11 11:23:20 +0800

    1111

---
+++ app/src/main/java/com/example/administrator/boxuegu/activity/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.example.administrator.boxuegu.activity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.administrator.boxuegu.MainActivity;
+import com.example.administrator.boxuegu.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+private TextView tv_version;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    private void init() {
+        tv_version=(TextView)findViewById(R.id.tv_version);
+        try {
+            PackageInfo info=getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+        //设置跳转时间
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/boxuegu/activity/main_title_bar.java
@@ -0,0 +1,15 @@
+package com.example.administrator.boxuegu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.boxuegu.R;
+
+public class main_title_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_title_bar);
+    }
+}
